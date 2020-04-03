2782c354fef3bf318e40a7db1dfeda5cb6584f78
breelynbetts <30633493+breelynbetts@users.noreply.github.com>
2020-04-02 17:56:14 -0700

    Update lengthOfLastWord.js

--- lengthOfLastWord.js
+++ lengthOfLastWord.js
@@ -2,7 +2,7 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function(s) {
+let lengthOfLastWord = function(s) {
     let arrOfWords = s.trim().split(" ");
     let lastword = arrOfWords.pop();
         
